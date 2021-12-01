7f08e7929fa6114292f3b9e21f94615bf42bc6ff
sf7com <sf7com22@gmail.com>
2021-12-01 16:46:14 +0900

    12월1일

---
+++ 데이터수집/API/1_네이버이미지.py
@@ -0,0 +1,90 @@
+#API 데이터 가져오는 절차
+#(1) 해당 사이트의 인증을 받는다.
+#(2) Service URL 팡ㄱ
+#(3) 입력 파라미터 무엇인지 파악
+#(4) 결과 데이터 형식이 어떤 것인지 파악
+#(5) 원하는 데이터만 가져오기
+id = 'jus1qXA5TBSvIew5VfrX'
+pw = 'J5Nq_LWHSY'
+
+#URL을 통해 서버에 데이터 요청 방법
+import urllib.request #URL을 통해서 웹 정보 가져오는 모듈
+import datetime
+import json
+import pprint
+import re
+# if response.getcode() == 200 : #정상응답
+#     with open('네이버.html','w',encoding='utf-8') as f :
+#         f.write(response.read().decode('utf-8'))
+
+def getRequestURL(url, decoding='utf-8') :
+    header = {'X-Naver-Client-Id':id, 'X-Naver-Client-Secret':pw}
+    req = urllib.request.Request(url, headers=header)
+    try : 
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200 :
+            print(f"[{datetime.datetime.now()}] URL Request Success")
+            return response.read().decode(decoding)
+    except Exception as e:
+        print(e)
+        print(f"[{datetime.datetime.now()}] Error for URL")
+
+def getNaverImage(searchStr, start, display,filter='small'):
+    serviceUrl = "https://openapi.naver.com/v1/search/image"
+    param = "?query=" + urllib.parse.quote(searchStr) #utf-8인코딩으로 변환
+    param += f"&display={display}&start={start}&filter={filter}"
+    url = serviceUrl+param
+    rtn = getRequestURL(url)
+    if rtn :
+        return json.loads(rtn)
+    else :
+        return None
+
+image = getNaverImage('고양이',1,100)
+print(image)
+
+#뉴스 1000건을 가져와서 저장하기
+searchStr = "고양이"
+itemList = []
+for start in range(1,1000,100) :
+    jsonResult = getNaverImage(searchStr,start,100)
+    for item in jsonResult['items'] :
+        itemDic = {}
+        itemDic['제목'] = re.sub(r'<.+?>', "", item['title']) 
+        itemDic['제목'] = re.sub(r'[^가-힣\da-zA-Z ]', "", itemDic['제목'])
+        itemDic['링크'] = item['link']
+
+        itemList.append(itemDic)
+
+pprint.pprint(itemList)
+print(f'가져온 뉴스 수 : {len(itemList)}')
+
+#이미지 저장
+import os
+path = './데이터수집/API/네이버 이미지'
+if not os.path.exists(path) :
+    os.makedirs(path)
+
+# import dload
+for idx,item in enumerate(itemList) :
+    title = item['제목']
+    title = title.strip() #문자열 앞뒤에 공백 지우기
+    link = item['링크']
+    fileName = str(idx)+"_"+title
+    #파일의 확장자 정보 찾기
+    m = re.search(r'\.(\w+?)$', link) #non-greedy : "?"" 최소한으로 가져오기
+    fileExtention = "jpg" if m==None else m.group(1)
+    fileName += "."+fileExtention
+    
+
+#링크정보로 데이터 다운로드시 dload
+#pip install dload
+    try :
+        filePath = path + "/" + fileName
+        # filePath = os.path.join(path, fileName)
+        # dload.save(link,filePath)
+        urllib.request.urlretrieve(link, filePath)
+        print(filePath)
+        print(f"{fileName} 다운로드 완료")
+    except :
+        print(f'[다운로드 오류]-{link}')
