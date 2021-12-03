5f767b3e9daa337a8b35e29f3c6ea61721306db0
sf7com <sf7com22@gmail.com>
2021-12-03 14:12:01 +0900

    업데이트

--- 라이브러리/WebCrawling.py
+++ 라이브러리/WebCrawling.py
@@ -2,8 +2,7 @@
 import urllib.request #URL을 통해서 웹 정보 가져오는 모듈
 import datetime
 def getRequestUrl(url, decoding='utf-8') :
-    header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
-    req = urllib.request.Request(url, headers=header)
+    req = urllib.request.Request(url)
     try :
         response = urllib.request.urlopen(req)
         if response.getcode() == 200 :
